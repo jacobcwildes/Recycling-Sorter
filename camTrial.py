8f5e613269511d1af595aa36b5e669ca6e74c507
ipekmelisturk <ipekmelisturk@gmail.com>
2023-11-16 15:28:37 -0500

    Camera tested and takes a picture, picam.py also records 5 second audio

---
+++ camTrial.py
@@ -0,0 +1,18 @@
+import cv2
+from pathlib import Path
+index = -1
+cap = cv2.VideoCapture(-1,2)
+cap.set(3, 640)
+cap.set(4, 480)
+# width = int(cap.get(3))
+# height = int(cap.get(4))
+
+
+while True:
+	ret, frame = cap.read()
+	cv2.imshow('frame', frame)
+	if cv2.waitKey(1) == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
--- picam.py
+++ picam.py
@@ -1,6 +1,7 @@
 #Import needed modules
 from picamera2 import Picamera2
 import cv2 as cv
+import os
 
 #Make a camera structure
 cap = Picamera2()
@@ -20,4 +21,10 @@ else:
 	while True:
 		im = cap.capture_array()
 		cv.imshow("Frame", im)
-		cv.waitKey(1)
+		if cv.waitKey(1) & 0xFF == ord('q'):
+			cv.imwrite('images/c1.png', im)
+			cv.destroyAllWindows()
+			break
+			
+cmd= "arecord -D hw:2,0 -d 5 -f cd ~/Desktop/471/project/test.wav -c 1"
+os.system(cmd)
